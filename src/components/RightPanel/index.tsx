c660b38175ad5b1faf7110df238d25cef7acbb74
colourful <colourful4570@163.com>
2025-01-02 15:29:03 +0800

    fix👽: json编辑器tab按键失效问题修复

--- src/components/RightPanel/index.tsx
+++ src/components/RightPanel/index.tsx
@@ -1,30 +1,68 @@
-import React, {useState} from 'react';
+import React, {useEffect} from 'react';
 import styles from './index.less';
 import {Tabs} from 'antd';
-import { JsonEditor as Editor } from 'jsoneditor-react';
+import {JsonEditor as Editor} from 'jsoneditor-react';
+import {useSelector} from 'dva';
 import 'jsoneditor-react/es/editor.min.css';
 
 const RightPanel = () => {
-    const [jsonData, setJsonData] = useState({"name":"hkp","age":18});
+    const currentComponent = useSelector((state: any) => state.project.currentComponent);
+
     const handleChangeJson = (data) => {
         console.log(data, 'data')
     }
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+        if (event.key === 'Tab') {
+            event.preventDefault();
+            // 获取当前的文本框
+            const textarea = event.target as HTMLTextAreaElement;
+            if (textarea && textarea.tagName === 'TEXTAREA') {
+                // 获取光标位置
+                const start = textarea.selectionStart;
+                const end = textarea.selectionEnd;
+
+                // 设置新的值并移动光标
+                textarea.value = textarea.value.substring(0, start)
+                    + '  ' // 插入Tab符，可能需要使用空格来代替
+                    + textarea.value.substring(end);
+
+                // 将光标移到Tab符之后
+                textarea.selectionStart =
+                    textarea.selectionEnd = start + 1; // +1 或者根据你的制表符宽度设置
+            }
+            return false;
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener('keydown', handleKeyDown);
+        return () => {
+            document.removeEventListener('keydown', handleKeyDown);
+        }
+    }, []);
+
     return (
         <div className={styles.right_panel}>
             <Tabs defaultActiveKey="dataSetting">
                 <Tabs.TabPane tab="基础设置" key="baseSetting">
                     基础设置
                 </Tabs.TabPane>
-                <Tabs.TabPane tab="数据" key="dataSetting">
+                <Tabs.TabPane tab="数据" key="dataSetting" style={{height: '600px'}}>
                     <Editor
-                        value={jsonData}
+                        value={currentComponent.data}
                         onChange={handleChangeJson}
                         mode={'code'}
+                        navigationBar={false}
+                        statusBar={false}
+                        search={false}
+                        history={false}
+                        sortObjectKeys={false}
+                        htmlElementProps={{className: 'json_editor'}}
                     />
                 </Tabs.TabPane>
             </Tabs>
         </div>
     );
 };
-
 export default RightPanel;
